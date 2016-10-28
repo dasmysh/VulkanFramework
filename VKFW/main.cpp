0b1818494fa67b0eeb8957e3d7029e97f5b1eb47
Sebastian Maisch <sebastian.maisch@uni-ulm.de>
2016-10-28 19:59:24 +0200

    Fixed config file and build.

--- VKFW/main.cpp
+++ VKFW/main.cpp
@@ -10,6 +10,7 @@
 #include "app_constants.h"
 #include "app/FWApplication.h"
 #include <g3log/logworker.hpp>
+#include <g3log/loglevels.hpp>
 
 #define WIN32_LEAN_AND_MEAN
 #define WIN32_EXTRA_LEAN
@@ -28,11 +29,23 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     auto worker = g3::LogWorker::createLogWorker();
     auto handle = worker->addDefaultLogger(name, directory);
 
-    LOG(INFO) << L"Log created.";
+    g3::only_change_at_initialization::setLogLevel(VK_GEN, true);
+    g3::only_change_at_initialization::setLogLevel(VK_INFO, true);
+    g3::only_change_at_initialization::setLogLevel(VK_ERROR, true);
+
+#ifndef NDEBUG
+    g3::only_change_at_initialization::setLogLevel(VK_DEBUG, true);
+    g3::only_change_at_initialization::setLogLevel(VK_WARNING, true);
+    g3::only_change_at_initialization::setLogLevel(VK_PERF_WARNING, true);
+#endif
+
+    g3::initializeLogging(worker.get());
+
+    LOG(INFO) << "Log created.";
 
     vkuapp::FWApplication app;
 
-    LOG(DEBUG) << L"Starting main loop.";
+    LOG(DEBUG) << "Starting main loop.";
     app.StartRun();
     auto done = false;
     while (app.IsRunning() && !done) {
@@ -40,12 +53,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
             app.Step();
         }
         catch (std::runtime_error e) {
-            LOG(FATAL) << L"Could not process frame step: " << e.what() << std::endl << L"Exiting.";
+            LOG(FATAL) << "Could not process frame step: " << e.what() << std::endl << "Exiting.";
             done = true;
         }
     }
     app.EndRun();
-    LOG(DEBUG) << L"Main loop ended.";
+    LOG(DEBUG) << "Main loop ended.";
 
     return 0;
 }
