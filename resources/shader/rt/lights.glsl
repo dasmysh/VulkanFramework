bc7b8122310586d955c709925fed985c9b5ab88c
Sebastian Maisch <sebastian.maisch@googlemail.com>
2023-12-21 14:45:55 +0100

    Current implementation.

--- resources/shader/rt/lights.glsl
+++ resources/shader/rt/lights.glsl
@@ -3,7 +3,7 @@
 
 #include "rayTraversal.glsl"
 
-vec3 next_event_estimation(vec3 origin, , in accelerationStructureEXT topLevelAS)
+vec3 next_event_estimation(vec3 origin, in accelerationStructureEXT topLevelAS)
 {
     const vec3 light_position = vec3(0.0f, 2.0f, 0.0f);
     const vec3 I = vec3(0.9882f, 0.0588f, 0.7529f);
@@ -13,7 +13,7 @@ vec3 next_event_estimation(vec3 origin, , in accelerationStructureEXT topLevelAS
 
     bool hit = queryShadowRayHit(origin, d, topLevelAS, 1.0f);
     if (!hit) {
-        return (I * brdf() * dot(normal, normalize(d))) / dot(d, d);
+        // return (I * brdf() * dot(normal, normalize(d))) / dot(d, d);
     }
     return vec3(0.0f);
 }
